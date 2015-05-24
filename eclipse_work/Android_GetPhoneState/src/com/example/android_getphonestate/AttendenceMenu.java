219e4fa962291a13ee80f444884098ca87034897
Saydous <rhwnrhs123@gmail.com>
2015-05-24 13:49:42 +0900

    Changing! - attendencemenu, showattendence

--- eclipse_work/Android_GetPhoneState/src/com/example/android_getphonestate/AttendenceMenu.java
+++ eclipse_work/Android_GetPhoneState/src/com/example/android_getphonestate/AttendenceMenu.java
@@ -1,54 +1,56 @@
 package com.example.android_getphonestate;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
-public class AttendenceMenu extends Activity implements OnClickListener {
-	Button btn1, btn2;
+public class AttendenceMenu extends ListActivity
+{
+	// 꼭 읽어주셔야 할 주의사항입니다.
+	// subjects에 첫번째, 즉 subjects[0]에는 반드시 '출석'이라는 글자가 들어가야 합니다.
+	// data 넣어주실때 꼭 생각해주시면 감사하겠습니다.
+	String subjects[]; // 과목 받으셔서 여기에 저장하시면 됩니다.
+	String date[]; // 날짜 저장하셔서 넘겨주실때 사용하시면 됩니다.
+	String temp1[]; // 다른 보내야할거 여기에 저장시켜주시면 됩니다. 변수는 바꿔주시면 되겠습니다.
+	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_attendencemenu);
-		btn1 = (Button)findViewById(R.id.attendBtn);
-		btn2 = (Button)findViewById(R.id.checkBtn);
-		btn1.setOnClickListener(this);
-		btn2.setOnClickListener(this);
+		setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, subjects));
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	@Override
-	public void onClick(View v) {
-		if(v.getId() == btn1.getId())
+	public void onListItemClick(ListView parent, View v, int position, long id)
+	{
+		if(position == 0)
 		{
 			startActivity(new Intent(this, Attend.class));
 		}
-		if(v.getId() == btn2.getId())
+		else
 		{
-			startActivity(new Intent(this, showAttendence.class));
+			Intent nextIntent = new Intent(this, showAttendence.class);
+			Bundle myData = new Bundle();
+			for(int i=0; i<date.length; i++)
+			{
+				myData.putString("date"+i, date[i]);
+			}
+			for(int i=0; i<temp1.length; i++)
+			{
+				myData.putString("temp1"+i, temp1[i]);
+			}
+			myData.putInt("dateCount", date.length);
+			myData.putInt("temp1Count", temp1.length);
+			nextIntent.putExtras(myData);
+			
+			startActivityForResult(nextIntent, 101);
 		}
 	}
 }
--- eclipse_work/Android_GetPhoneState/src/com/example/android_getphonestate/showAttendence.java
+++ eclipse_work/Android_GetPhoneState/src/com/example/android_getphonestate/showAttendence.java
@@ -1,17 +1,40 @@
 package com.example.android_getphonestate;
 
 import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
-public class showAttendence extends Activity{
+public class showAttendence extends ListActivity{
 
-	TextView attend;
+	String getDate[];
+	String getTemp1[];
+	String hap[];
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_showattendence);
-
-		attend = (TextView) findViewById(R.id.stuattend);
+			
+		Intent getDataIntent = getIntent();
+		
+		Bundle myBundle = getDataIntent.getExtras();
+		hap = new String[myBundle.getInt("dateCount")];
+		
+		for(int i=0; i<myBundle.getInt("dateCount"); i++)
+		{
+			hap[i] = myBundle.getString("date"+i);
+		}
+		
+		for(int i=0; i<myBundle.getInt("temp1Count"); i++)
+		{
+			hap[i] = hap[i] + " " + myBundle.getString("temp1"+i);
+		}
+		
+		setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, hap));
 	}
 }
