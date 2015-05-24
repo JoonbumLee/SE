244dac4eee64dadb60f0c400783ebae6d384f0c1
choiyoonho2 <chldbsgh112@naver.com>
2015-05-24 15:35:03 +0900

    pro

--- Android_GetPhoneState/src/com/example/android_getphonestate/ProAttendenceMenu.java
+++ Android_GetPhoneState/src/com/example/android_getphonestate/ProAttendenceMenu.java
@@ -3,46 +3,41 @@ package com.example.android_getphonestate;
 import java.util.ArrayList;
 
 import android.app.Activity;
-import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
-public class ProAttendenceMenu extends Activity implements OnClickListener {
+public class ProAttendenceMenu extends Activity implements AdapterView.OnItemClickListener {
 
 	Button btn1, btn2;
 	ArrayAdapter<String> adapter; 
 	ListView listView; 
+	String select, select2;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_proattendencemenu);
-		btn1 = (Button)findViewById(R.id.AttendenceCheck);
-		btn2 = (Button)findViewById(R.id.AllAttendence);
-		btn1.setOnClickListener(this);
-		btn2.setOnClickListener(this);
-		
+		// 실제로는 강의리스트를 Login 액티비티에서 전달 받아서 표현 해 줘야 함
 		ArrayList<String> courselist = new ArrayList<String>();
 		courselist.add("알고리즘");
 		courselist.add("소프트웨어공학");
-		//listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE); 
-		adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_single_choice,courselist);
+		adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,courselist);
 		listView = (ListView) findViewById(R.id.courselist);
 		listView.setAdapter(adapter);
-}
+		listView.setOnItemClickListener(this);
+	}
+
 	@Override
-	public void onClick(View v) {
-		if(v.getId() == btn1.getId())
-		{
-			startActivity(new Intent(this, ProOneAttend.class));
-		}
-		if(v.getId() == btn2.getId())
-		{
-			startActivity(new Intent(this, ProAllAttend.class));
-		}
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) {
+		// 강의 리스트 중 하나가 터치되면 그 강의가 있었던 날짜를 다음 액티비티로 전달한다.
+		Intent intent = new Intent(ProAttendenceMenu.this, ProOneAttend.class);
+		intent.putExtra("Date","5월24일");
+		startActivity(intent);
+		
 	}
 }
